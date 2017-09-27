9cdb205ce92a5da77114909b420bf174cf822db4
Multy Pony <meshcheryakovvrn@gmail.com>
2017-09-27 20:48:42 +0300

    some fixes

--- coins.js
+++ coins.js
@@ -48,32 +48,8 @@ function amountToCoins(amount, coins) {
   if(sum != amount) return [sum, round((amount - sum), 2)];
   return [resOb, sum];
 }
-/*
-function amountToCoins(amount, coins) {
-  let sum = 0;
-  let countDen = 0;
-  let resOb = {};
-  let keys = Object.keys(coins);
-  for(let i = 0; sum < amount && i < keys.length; ++i) {
-    // console.log('i = ' + i + ' amount - sum = ' + (amount - sum));
-    countDen = (amount - sum) / den[keys[i]];
-    countDen = countDen < 1 ? 0 : round(countDen, 0);
-    // console.log('i = ' + i + ' countDen = ' + countDen + ' den[keys[i]] = '+den[keys[i]]);
-    if(countDen < coins[keys[i]]) {
-      sum += countDen * den[keys[i]];
-      resOb[keys[i]] = countDen;
-    } else {
-      sum += coins[keys[i]] * den[keys[i]];
-      resOb[keys[i]] = coins[keys[i]];
-    }
-  }
-  if(sum != amount) return [sum, round((amount - sum), 2)];
-  // return new Array(resOb, sum);
-  return [resOb, sum];
-}
-*/
 
-console.log(coinsToAmount({qs: 4, ns: 5, ps: 10, ds: 20}));
-console.log(amountToCoins(2, {qs: 7}));
-console.log(amountToCoins(5.23, {qs: 20, ds: 5, ns: 10, ps: 20}));
-console.log(amountToCoins(7, {qs: 20, ds: 5, ns: 10, ps: 20}));
+console.log(coinsToAmount({qs: 4, ns: 5, ps: 10, ds: 20}));         //3.35
+console.log(amountToCoins(2, {qs: 7}));                             //[ 1.75, 0.25 ]
+console.log(amountToCoins(5.23, {qs: 20, ds: 5, ns: 10, ps: 20}));  //[ { qs: 20, ds: 2, ps: 3 }, 5.23 ]
+console.log(amountToCoins(7, {qs: 20, ds: 5, ns: 10, ps: 20}));     //[ 6.2, 0.8 ]
